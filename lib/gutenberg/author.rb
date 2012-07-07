5626f68d9d8a4ed17c86375b16240bafed970010
Constantin Hofstetter <Consti@Consti.de>
2012-07-07 04:53:14 +0200

    fix author_id in Author

--- lib/gutenberg/author.rb
+++ lib/gutenberg/author.rb
@@ -5,24 +5,20 @@ module Gutenberg
 
     def initialize(rdf_reader)
       rdf_reader.each_statement do |statement|
-        if statement_is_an_author?(statement)
-          parse_author(statement)
-        end
+        parse_author(statement)
       end
     end
 
     protected
 
     def statement_is_creator?(statement)
-      Regexp.new(
-        %w(creator).join('$|')).
-      match(statement.predicate.to_s) &&
-      Regexp.new(/\/agents\/\d+/).match(statement.subject.to_s)
+      Regexp.new(/\/creator$/).match(statement.predicate.to_s) &&
+      Regexp.new(/\/agents\/\d+/).match(statement.object.to_s)
     end
 
     def statement_is_an_author?(statement)
       what_from_predicate(statement.predicate.to_s) &&
-      Regexp.new(/\/agents\/#{ @author_id }+/).match(statement.subject.to_s)
+      Regexp.new(/\/agents\/#{ @author_id }/).match(statement.subject.to_s)
     end
 
     def what_from_predicate(predicate)
@@ -33,8 +29,8 @@ module Gutenberg
 
     def parse_author(statement)
       if statement_is_creator?(statement)
-        @creator = statement.object.to_s
-      else
+        @author_id = statement.object.to_s.match(/\/(\d+)$/)[1]
+      elsif statement_is_an_author?(statement)
         if what = what_from_predicate(statement.predicate.to_s)
           self.send("#{ what }=", statement.object.to_s)
         end
