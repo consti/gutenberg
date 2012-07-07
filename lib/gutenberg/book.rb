8ffe014f5d40712dab26687d847f99543aea3662
Constantin Hofstetter <Consti@Consti.de>
2012-07-07 02:57:48 +0200

    extend Book

--- lib/gutenberg/book.rb
+++ lib/gutenberg/book.rb
@@ -1,30 +1,48 @@
 module Gutenberg
   class Book
-    attr_accessor :book_id, :creator, :title, :friendlytitle, :publisher, :rights
+    attr_accessor :book_id, :creator, :title, :friendlytitle, :publisher, :rights, :license, :issued
     attr_reader   :rdf_reader, :downloads, :author
 
-    def initialize(book_id, creator=nil, title=nil, friendlytitle=nil, publisher=nil, rights=nil)
-      @creator        = creator
-      @title          = title
-      @friendlytitle  = friendlytitle
-      @publisher      = publisher
-      @rights         = rights
-      @book_id        = book_id
+    def initialize(book_id)
+      @book_id = book_id
     end
 
     def get_data
       @author ||= Gutenberg::Author.new(rdf_reader)
+      @creator = @author.name
 
-      download_urls = rdf_reader.statements.map { |statement|
-        download_link(statement)
-      }.compact.uniq
+      download_urls = []
 
-      @downloads = download_urls.map do |download_url|
+      rdf_reader.each_statement do |statement|
+        download_urls << download_link(statement)
+        if statement_is_book?(statement)
+          parse_book(statement)
+        end
+      end
+
+      @downloads = download_urls.compact.uniq.map do |download_url|
         Gutenberg::Download.new(rdf_reader, download_url)
       end
     end
 
     protected
+
+     def parse_book(statement)
+      if what = what_from_predicate(statement.predicate.to_s)
+        self.send("#{ what }=", statement.object.to_s)
+      end
+    end
+
+    def statement_is_book?(statement)
+      what_from_predicate(statement.predicate.to_s) &&
+      Regexp.new(/\/ebooks\/#{ @book_id }/).match(statement.subject.to_s)
+    end
+
+    def what_from_predicate(predicate)
+      Regexp.new(
+          %w(rights publisher license title issued).join('$|')).
+        match(predicate)
+    end
 
     def download_link(statement)
       return unless Regexp.new(/isFormatOf$/).match(statement.predicate.to_s)
